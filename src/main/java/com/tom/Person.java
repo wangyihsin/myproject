b5c914afecf82cb0a482bc19d58fe4250230be23
wangyihsin <qazwsx831234@yahoo.com.tw>
2019-06-07 20:17:04 +0800

    first test

---
+++ src/main/java/com/tom/Person.java
@@ -0,0 +1,7 @@
+package com.tom;
+
+public class Person {
+    public void Hello(){
+        System.out.println("Hello world");
+    }
+}
---
+++ src/main/kotlin/com/tom/Hello.kt
@@ -0,0 +1,15 @@
+package com.tom
+
+fun main() {
+//    println("HELLO KOTLIN")
+//    Human().Hello()
+    val h = Human()
+    h.hello()
+
+}
+
+class Human{
+    fun hello(){
+        println("hello kotlin2")
+    }
+}
